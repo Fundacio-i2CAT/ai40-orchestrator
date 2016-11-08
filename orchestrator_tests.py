7ae6835ec2722b58eba67ab612cb1832f89843d5
Alfonso Egio <alfonso.egio@gmail.com>
2016-11-08 09:21:11 +0100

    start-stop tests

--- orchestrator_tests.py
+++ orchestrator_tests.py
@@ -2,38 +2,41 @@
 # -*- coding: utf-8 -*-
 """Anella 4.0 Orchestrator Tests"""
 
-from start import PORT, URL_PREFIX, APP
+from start import PORT, URL_PREFIX
 import json
 import unittest
 import requests
+import threading
+import functools
 
-BASE_URL = 'http://localhost:{0}{1}'.format(PORT, URL_PREFIX)
+BASE_URL = 'http://dev.anella.i2cat.net:{0}{1}'.format(PORT, URL_PREFIX)
 
 class OrchestratorTestCase(unittest.TestCase):
     """Full test"""
 
     def setUp(self):
         """Initial setup"""
-        self.app = APP
+        pass
 
     def step1(self):
         """Post NS instance"""
         with open('tenor_client/samples/another.json') as infile:
             rdata = infile.read()
-        resp = requests.post('{0}/service/instance'.format(BASE_URL),
-                      headers={'Content-Type': 'application/json'},
-                      json=json.loads(rdata))
-        assert resp.status_code == 200
-        data = json.loads(resp.text)
-        assert 'service_instance_id' in data
-        assert data['state'].upper() == 'PROVISIONED'
+        # resp = requests.post('{0}/service/instance'.format(BASE_URL),
+        #               headers={'Content-Type': 'application/json'},
+        #               json=json.loads(rdata))
+        # assert resp.status_code == 200
+        # data = json.loads(resp.text)
+        # assert 'service_instance_id' in data
+        # assert data['state'].upper() == 'PROVISIONED'
 
     def step2(self):
         """Gets NS instances"""
         resp = requests.get('{0}/service/instance'.format(BASE_URL))
         assert resp.status_code == 200
         instances = json.loads(resp.text)
-        assert 'service_instance_id' in instances[0]
+        if len(instances) > 0:
+            assert 'service_instance_id' in instances[0]
         return instances
 
     def step3(self):
@@ -41,35 +44,38 @@ class OrchestratorTestCase(unittest.TestCase):
         instances = self.step2()
         running_instances = [x for x in instances if x['state'].upper() == 'RUNNING']
         for ins in running_instances:
-            stop_resp = requests.put('{0}/service/instance/{1}'.format(
-                BASE_URL,
-                ins['service_instance_id'],
-                headers={'Content-Type': 'application/json'},
-                json=json.loads('{"state": "deployed"}')))
+            url = '{0}/service/instance/{1}'.format(BASE_URL,
+                                                    ins['service_instance_id'])
+            stop_resp = requests.put(url,
+                                     headers={'Content-Type': 'application/json'},
+                                     json={'state': 'deployed'})
             assert stop_resp.status_code == 200
 
     def step4(self):
-        """Starts all NS instances"""
+        """Starts all NS stopped instances"""
         instances = self.step2()
         deployed_instances = [x for x in instances if x['state'].upper() == 'DEPLOYED']
         for ins in deployed_instances:
-            stop_resp = requests.put('{0}/service/instance/{1}'.format(
-                BASE_URL,
-                ins['service_instance_id'],
-                headers={'Content-Type': 'application/json'},
-                json=json.loads('{"state": "deployed"}')))
-            assert stop_resp.status_code == 200
+            url = '{0}/service/instance/{1}'.format(BASE_URL,
+                                                    ins['service_instance_id'])
+            start_resp = requests.put(url,
+                                     headers={'Content-Type': 'application/json'},
+                                     json={'state': 'start'})
+            assert start_resp.status_code == 200
 
     def step5(self):
         """Posts a new VNF"""
+        pass
         
 
     def _steps(self):
+        """Steps"""
         for name in sorted(dir(self)):
             if name.startswith("step"):
                 yield name, getattr(self, name)
 
     def test_steps(self):
+        """Sorting steps"""
         for name, step in self._steps():
             print name, step.__doc__
             try:
